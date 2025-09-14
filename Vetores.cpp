ab02d794ceb242ce86e25b7959f90cbad5c8a556
Gabriel Pereira <gabrielcavalcantip7@gmail.com>
2025-09-14 20:01:56 -0300

    t

---
+++ Vetores.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX_VETOR 50
+
+void InserirOrdenado(int vetor[], int tam, int x){
+    int pos = tam;
+    while (pos > 0 && x < vetor[pos-1]){
+
+    }
+}
+
+
+
+g 
+
+
+
+
+
+
+
+
+int main(){
+
+    return 0;
+}
