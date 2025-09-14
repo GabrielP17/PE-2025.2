58b9fec22eb024eabc5f9ca987e6268756294b51
Gabriel Pereira <gabrielcavalcantip7@gmail.com>
2025-09-14 20:18:58 -0300

    teste

--- Vetores.cpp
+++ Vetores.cpp
@@ -5,14 +5,48 @@
 
 void InserirOrdenado(int vetor[], int tam, int x){
     int pos = tam;
-    while (pos > 0 && x < vetor[pos-1]){
-
+    while (pos > 0 && x < vetor[pos-1]) {
+        vetor[pos] = vetor[pos - 1];
+        pos -= 1;
     }
+    vetor[pos] = x;
+}
+
+void ImprimirVetor(int vetor[], int tamanho){
+    if (tamanho = 0){
+        printf("vetor está vazio\n");
+        return;
+    }
+
+    printf("%d elementos", tamanho);
+    for(int i = 0; i < tamanho; i++){
+        printf("%d", vetor[i]);
+        if (i < tamanho-1 ){
+            printf(", ");
+        }
+    }
+    printf("]\n");
+}
+
+int BuscaBinaria(int vetor[], int tamanho, int valor){
+    int inicio = 0;
+    int fim = tamanho - 1;
+
+    while (inicio <= fim) {
+        int meio = (inicio + fim) /2;
+
+        if (vetor[meio] == valor){
+            return meio;
+        } else if(vetor[meio] < valor){
+            inicio = meio + 1;
+        }else {
+            fim = meio - 1;
+        }
+    }
+    return -1;
 }
 
 
-
-g 
 
 
 
